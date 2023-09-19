cfd89b7fce8b406a685d45751bfacf77ac356a38
xin625201 <xin625201@126.com>
2023-09-19 11:13:40 +0800

    create demo

---
+++ SnapKitDemo/SnapKitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKitDemo
+//
+//  Created by xin on 2023/9/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+    }
+
+
+}
+
